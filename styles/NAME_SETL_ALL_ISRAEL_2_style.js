1865d012bfcad5257b344db6759df9201ac1e5df
idshklein <idshklein@gmail.com>
2023-10-20 09:44:39 +0300

    qgis2web open layers

---
+++ styles/NAME_SETL_ALL_ISRAEL_2_style.js
@@ -0,0 +1,32 @@
+var size = 0;
+var placement = 'point';
+
+var style_NAME_SETL_ALL_ISRAEL_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "22.1px \'Arial\', sans-serif";
+    var labelFill = "#e3ffff";
+    var bufferColor = "#000000";
+    var bufferWidth = 2.000000000000001;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("SETL_NAME") !== null) {
+        labelText = String(feature.get("SETL_NAME"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(183,72,75,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
